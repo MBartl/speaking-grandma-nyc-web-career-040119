5a9204727df1fc9cea22ed9123b899f36f4265da
Matthew Bartl <mattbartl@gmail.com>
2019-03-18 01:31:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(string)
-  if string.upcase
+def speak_to_grandma(said)
+  if said.upcase
     return "NO, NOT SINCE 1938"
-  elsif string == "I LOVE YOU GRANDMA!"
+  elsif said == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
